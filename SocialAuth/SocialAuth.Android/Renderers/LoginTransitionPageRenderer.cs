3ea7b1273659c7d737f9b55e491e32deae3bf87e
Wilson <wilsonvargas_6@outlook.com>
2017-11-13 20:19:02 -0500

    First commit

---
+++ SocialAuth/SocialAuth.Android/Renderers/LoginTransitionPageRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using SocialAuth.Droid.Renderers;
+using SocialAuth.Views;
+using SocialAuth.Resources.Facebook;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Auth;
+using SocialAuth.Models;
+
+[assembly: ExportRenderer(typeof(LoginTransitionPage), typeof(LoginTransitionPageRenderer))]
+namespace SocialAuth.Droid.Renderers
+{
+    public class LoginTransitionPageRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            base.OnElementChanged(e);
+
+            // this is a ViewGroup - so should be able to load an AXML file and FindView<>
+            var activity = this.Context as Activity;
+
+            var auth = new OAuth2Authenticator(
+                clientId: Variables.ClientId, // your OAuth2 client id
+                scope: Variables.Scope, // The scopes for the particular API you're accessing. The format for this will vary by API.
+                authorizeUrl: new Uri(Variables.AuthorizeUrl), // the auth URL for the service
+                redirectUrl: new Uri(Variables.RedirectUrl)); // the redirect URL for the service
+
+            auth.Completed += (sender, eventArgs) => {
+                if (eventArgs.IsAuthenticated)
+                {
+                    var token = eventArgs.Account.Properties["access_token"];
+                    App.Instance.NavigateToMain();
+                }
+                else
+                {
+                    // The user cancelled
+                }
+            };
+
+            activity.StartActivity(auth.GetUI(activity));
+        }
+    }
+}
---
+++ SocialAuth/SocialAuth/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using SocialAuth.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SocialAuth
+{
+    public partial class App : Application
+    {
+        static App _Instance;
+        public App()
+        {
+            InitializeComponent();
+            _Instance = this;
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        public static App Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        public void NavigateToMain() {
+            _Instance.MainPage = new MainPage();
+        }
+    }
+}
